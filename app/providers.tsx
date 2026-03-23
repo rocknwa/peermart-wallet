486ab0946246fb75f738f0b1445e8981d29af816
Therock Ani <118687295+rocknwa@users.noreply.github.com>
2026-03-23 16:05:43 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { AlchemyAccountProvider } from "@account-kit/react";
+import { AlchemyAccountProvider, type AlchemyAccountsProviderProps } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { config, queryClient } from "@/lib/alchemy-config";
+import type { PropsWithChildren } from "react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers(
+  props: PropsWithChildren<{ initialState?: AlchemyAccountsProviderProps["initialState"] }>
+) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AlchemyAccountProvider config={config} queryClient={queryClient}>
-        {children}
+      <AlchemyAccountProvider
+        config={config}
+        queryClient={queryClient}
+        initialState={props.initialState}
+      >
+        {props.children}
       </AlchemyAccountProvider>
     </QueryClientProvider>
   );
